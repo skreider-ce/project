2757eb4452e65328c3aa6814eedacccfac191f84
yolandamunoz <90872949+yolandamunoz@users.noreply.github.com>
2025-02-07 10:09:07 -0600

    checking Nicole's function

---
+++ R/Nicole_function_check_YM.R
@@ -0,0 +1,23 @@
+# Program: Nicole_function_check_YM.R
+# Author:  Yolanda Munoz Maldonado
+# Date:    02/07/2025
+# Purpose: Run Nicole's function
+#----------------------------------------------------------------
+
+library(docxtractr)
+library(huxtable)
+library(dplyr)
+library(utils)
+library(officer)
+library(glue)
+library(flextable)
+
+source("./R/fill_registry_qc_doc.R")
+qc_checklist_path <- "C:/Users/munozmy/Corrona LLC/Biostat and Epi Team Site - Documents//Biostat QC/Biostat Registry - Monthly and Quarterly Report QC Checklist.docx"
+
+result_path <- "C:/Users/munozmy/Documents/R/R package SG"
+
+fill_registry_qc_doc( qc_checklist_path,
+                     results_folder = result_path,
+                     final_name = "Filled QC Registry QC Checklist_YM",
+                     analyst_initials = "YMM")
