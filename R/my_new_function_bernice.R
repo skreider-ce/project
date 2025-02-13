c8c89bf5392a4d6dfed8fe045a92d991584fe470
Bernice Gershenson <79602589+bernicegershenson@users.noreply.github.com>
2025-02-13 11:17:38 -0500

    Updates

--- R/my_new_function_bernice.R
+++ R/my_new_function_bernice.R
@@ -1,16 +1,15 @@
     #' @name my_new_function
     #' @title Takes dataset and gives us mean value between two variables
     #'
-    #' @param data Any input dataset
+    #' @param data Any input data
     #' @param var1 Name of the first variable
     #' @param var2 Name of the second variable
     #'
     #' @return the variable mean
     #' @export
-    #' @importFrom dplyr summarize
+    #' @importFrom dplyr summarize %>%
     #' @examples
     #' corproject::my_new_function(cars, speed, dist)
-library(dplyr)
 my_new_function<- function(data, var1, var2) {
   data %>%
     summarize(
