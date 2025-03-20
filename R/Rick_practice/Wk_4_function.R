0cd55a207c208a7eb2f4c8e497f14cfa1fd134dc
m-gurrola <mgurrola@corevitas.com>
2025-03-20 12:22:00 -0600

    Merging main into my dev branch

---
+++ R/Rick_practice/Wk_4_function.R
@@ -0,0 +1,38 @@
+#' Misspell generator
+#'
+#' @param word A word to misspell
+#' @param start_pos The position of the first letter you'd like swapped
+#' @param split_num The distance between the next the first and second letter you'd like swapped
+#'
+#' @return A misspelled word
+#' @export
+#'
+#' @examples misspell_swap("test", 1,2)
+
+
+
+#Random Character Swap:  Swap two adjacent characters
+
+misspell_swap <- function(word, start_pos, split_num) {
+  word_length <- nchar(word)
+  
+  # Check for errors
+  if (start_pos > word_length || split_num > word_length || (start_pos + split_num) > word_length) {
+    return("Word length cannot accomodate your parameters")
+  }
+  
+  # Ensure the position to swap is valid
+  pos <- start_pos
+  chars <- strsplit(word, NULL)[[1]]
+  
+  # Swap characters
+  temp <- chars[pos]
+  chars[pos] <- chars[pos + split_num]
+  chars[pos + split_num] <- temp
+  
+  return(paste(chars, collapse = ""))
+}
+
+
+
+misspell_swap("information", 1,3)
---
+++ R/bernice_gershenson/practicefunction.R
@@ -0,0 +1,49 @@
+install.packages("dplyr")
+install.packages("officer")
+install.packages("glue")
+library(dplyr)
+library(officer)
+library(glue)
+library(haven)
+
+#Step1- to see if basic function works.
+read_dataset <- function() {
+  print("read_dataset")
+}
+
+read_dataset()
+
+#step2
+read_dataset <- function(ds_url) {
+  print(ds_url)
+}
+analysis_dataset=read_dataset("url")
+analysis_dataset=read_dataset("dsmadma")
+
+
+#step3
+
+read_dataset <- function(ds_url) {
+  print(ds_url)
+  temp_ds= base::readRDS(ds_url)
+
+  return(temp_ds)
+}
+analysis_dataset=read_dataset("C:/Users/bgershenson/Corrona LLC/Biostat Data Files - AD/monthly/2025/2025-01-03/exvisit_2025-01-03.rds")
+analysis_dataset=read_dataset("C:/Users/bgershenson/Corrona LLC/Biostat Data Files - AD/monthly/2025/2025-01-03/exinstances_2025-01-03.rds")
+
+#step4
+
+summarize_dataset<- function(dataset){
+  temp_summary_table<- base::summary(dataset)
+
+  return(temp_summary_table)
+}
+
+summary_table=summarize_dataset(analysis_dataset)
+
+str(analysis_dataset)
+###Summarize all the steps above.
+
+
+
